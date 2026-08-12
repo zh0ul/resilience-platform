e88a2e81100ae7e25e852fae7eabe0135723157c
zh0ul <lentz_e@hotmail.com>
2026-08-11 21:45:33 -0700

    update run_offline_checks

--- scripts/run_offline_checks.bat
+++ scripts/run_offline_checks.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
+ECHO === Running Pytest tests ===
+ .\.venv\Scripts\pytest.exe tests/unit tests/rest -v
+ECHO.
 ECHO === Running Ruff checks ===
 .\.venv\Scripts\ruff.exe check .
 ECHO.
 ECHO === Running MyPy checks ===
 .\.venv\Scripts\mypy.exe src
-ECHO.
-ECHO === Running Pytest tests ===
- .\.venv\Scripts\pytest.exe tests/unit tests/rest -v
